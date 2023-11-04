423d429ca996089d5453c8c6ca65c17ea4706bc0
Aaryan Mahajan <146549440+realhaxaaryan@users.noreply.github.com>
2023-11-04 09:16:40 +0530

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i=1;
+    do
+    {
+      printf("harsh is eating alone\n");
+      i++;
+    } 
+    while(i<=10);
+    return 0;
+}
---
+++ advanced2.c
@@ -0,0 +1,17 @@
+//wap to display even nos from 0-14
+#include<stdio.h>
+void main(){
+
+    int i,a;
+    for(i=0;i=15;i++)
+    if(i%2==0)
+    {
+        printf("the reqd even nos are %d",i);
+    }
+    else
+    {
+        printf ("no nos are even");
+    }
+
+
+}
